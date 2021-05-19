975c3e8b869f1032c505f16b8a55680bb42086bf
Daisy <misaki_yachu@yahoo.com.tw>
2021-05-19 22:28:27 +0800

    edit style css/ handlebars

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { body, validationResult } = require('express-validator/check')
+const { body, validationResult } = require("express-validator")
 const Record = require('../../models/record')
 const router = express.Router()
 
